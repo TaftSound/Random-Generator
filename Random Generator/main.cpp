f85bed28380781e40316cb74cfc81f27bcc81813
TaftSound <mattmurphytattoos@gmail.com>
2022-05-10 11:02:04 -0700

    Initial Commit

---
+++ Random Generator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Random Generator
+//
+//  Created by Matthew Murphy on 5/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
